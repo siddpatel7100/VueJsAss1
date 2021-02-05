1489dd364a4c8f5bf0a850ba545cbc5ae30ae674
Chinmay Shah <Cshah5582@conestogac.on.ca>
2021-02-06 03:22:50 +0530

    login added

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,5 +1,20 @@
 <template>
 <header>
+  <mdb-container left>
+    <mdb-btn color="default" @click.native="login=true">Login<mdb-icon icon="eye" class="m1-1"/></mdb-btn>
+      <mdb-modal :show="login" @close="login=false">
+        <mdb-modal-header class="text-center">
+          <mdb-modal-title tag="h4" bold class="w-100">Login</mdb-modal-title>
+        </mdb-modal-header>
+        <mdb-modal-body class="mx-3 grey-text">
+          <mdb-input label="Your Email" icon="envelope" type="email" class="mb-5"/>
+          <mdb-input label="Your Password" icon="lock" type="password"/>
+        </mdb-modal-body>
+        <mdb-modal-footer center>
+          <mdb-btn @click.native="login=false">Login</mdb-btn>
+        </mdb-modal-footer>
+      </mdb-modal>
+  </mdb-container>
   <b-nav>
 
     <b-nav-item ><router-link to="/tableview" >Table View</router-link></b-nav-item>
@@ -11,8 +26,25 @@
 </template>
 
 <script>
+import{ mdbContainer,mdbBtn,mdbIcon,mdbModal,mdbModalHeader,mdbModalBody,mdbModalFooter,mdbInput,mdbModalTitle} from 'mdbvue';
 export default {
-name: "header"
+name: "header",
+  components:{
+    mdbContainer,
+    mdbBtn,
+    mdbModal,
+    mdbModalHeader,
+    mdbModalBody,
+    mdbModalFooter,
+    mdbInput,
+    mdbModalTitle,
+    mdbIcon
+  },
+  data(){
+    return{
+      login:false
+    }
+  }
 }
 </script>
 
