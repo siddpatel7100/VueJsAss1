88793e834cdfa3a3e9ae62fddd9f357ab5645001
Chinmay Shah <Cshah5582@conestogac.on.ca>
2021-02-07 16:48:48 +0530

    login and registration done

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,17 +1,39 @@
 <template>
 <header>
-  <mdb-container left>
-    <mdb-btn color="default" @click.native="login=true">Login<mdb-icon icon="eye" class="m1-1"/></mdb-btn>
-      <mdb-modal :show="login" @close="login=false">
-        <mdb-modal-header class="text-center">
-          <mdb-modal-title tag="h4" bold class="w-100">Login</mdb-modal-title>
-        </mdb-modal-header>
-        <mdb-modal-body class="mx-3 grey-text">
-          <mdb-input label="Your Email" icon="envelope" type="email" class="mb-5"/>
-          <mdb-input label="Your Password" icon="lock" type="password"/>
+  <mdb-container>
+    <mdb-btn rounded color="default" @click.native="cascading=true">Login/Register<mdb-icon icon="eye" class="ml-1"/></mdb-btn>
+      <mdb-modal :show="cascading" @close="cascading=false" cascade tabs>
+        <mdb-tab tabs justify class="light-blue darken-3">
+          <mdb-tab-item :active="tabs==1" @click.native.prevent="tabs=1">Login</mdb-tab-item>
+          <mdb-tab-item :active="tabs==2" @click.native.prevent="tabs=2">Register</mdb-tab-item>
+        </mdb-tab>
+        <mdb-modal-body class="mx-3" v-if="tabs==1">
+          <mdb-input label="Your Email" type="email" class="mb-5"/>
+          <mdb-input label="Your Password" type="password"/>
+          <div class="mt-2 text-center">
+            <mdb-btn color="info">Log In<mdb-icon icon="sign-in-alt" class="ml-1"/></mdb-btn>
+          </div>
         </mdb-modal-body>
-        <mdb-modal-footer center>
-          <mdb-btn @click.native="login=false">Login</mdb-btn>
+        <mdb-modal-footer center v-if="tabs==1">
+          <div class="options text-center text-md-right mt-1">
+            <p>Not a Member?<a href="#" @click="tabs=2">Sign Up</a></p>
+          </div>
+          <mdb-btn outline="info" class="ml-auto" @click.native="cascading=false">Close</mdb-btn>
+        </mdb-modal-footer>
+        <mdb-modal-body class="mx-3" v-if="tabs==2">
+          <mdb-input label="Your Email"  type="email" class="mb-5"/>
+          <mdb-input label="Your Username"  class="mb-5"/>
+          <mdb-input label="Your Password"  type="password" class="mb-5"/>
+          <mdb-input label="Repeat Password" type="password"/>
+          <div class="mt-2 text-center">
+            <mdb-btn color="info">Sign Up<mdb-icon icon="sign-in-alt" class="ml-1"/></mdb-btn>
+          </div>
+        </mdb-modal-body>
+        <mdb-modal-footer center v-if="tabs==2">
+          <div class="options text-center text-md-right mt-1">
+            <p>Already have an account?<a href="#" @click="tabs=1">Login</a></p>
+          </div>
+          <mdb-btn outline="info" class="ml-auto" @click.native="cascading=false">Close</mdb-btn>
         </mdb-modal-footer>
       </mdb-modal>
   </mdb-container>
@@ -26,23 +48,24 @@
 </template>
 
 <script>
-import{ mdbContainer,mdbBtn,mdbIcon,mdbModal,mdbModalHeader,mdbModalBody,mdbModalFooter,mdbInput,mdbModalTitle} from 'mdbvue';
+import{ mdbContainer,mdbBtn,mdbModal,mdbTab,mdbTabItem,mdbModalBody,mdbInput,mdbModalFooter,mdbIcon} from 'mdbvue';
 export default {
 name: "header",
   components:{
     mdbContainer,
     mdbBtn,
     mdbModal,
-    mdbModalHeader,
+    mdbTab,
+    mdbTabItem,
     mdbModalBody,
-    mdbModalFooter,
     mdbInput,
-    mdbModalTitle,
-    mdbIcon
+    mdbModalFooter,
+    mdbIcon,
   },
   data(){
     return{
-      login:false
+      cascading:false,
+      tabs:1
     }
   }
 }
